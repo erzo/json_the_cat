20e20d5bb2cfcdbc4f654d6f21d19b18c5b040f1
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-02 22:50:26 +0000

    modified

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,26 @@
 const request = require('request');
 
-let myArgs = process.argv.slice(2);
+// errors and data validation are two different concerns
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${myArgs}`, function(error,response,body) {
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error,response,body) {
   // console.log(body);
   // console.log(body[0]);
   const data = JSON.parse(body);
   // console.log(data[0]);
-  if (data.length === 0) {
-    console.log('please try again');
-  } else {
-    console.log(data[0].description);
-  }
-});
 
-// errors and data validation are two different concerns
+    // if (data.length === 0) {
+    //   console.log('please try again');
+    // } else {
+    //     console.log(data[0].description);
+    //   }
 
+    callback(error, data[0].description);
 
+  });
+
+};
+
+module.exports = {fetchBreedDescription};
