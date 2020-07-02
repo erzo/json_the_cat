5fe3b052935c41022ab3895793a2c9d07fc76874
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-02 22:02:42 +0000

    code that pulls information from website based off arguement taken from command line

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,4 +1,19 @@
 const request = require('request');
 
-console.log(request);
+let myArgs = process.argv.slice(2);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${myArgs}`, function(error,response,body) {
+  // console.log(body);
+  // console.log(body[0]);
+  const data = JSON.parse(body);
+  // console.log(data[0]);
+  if (data.length === 0) {
+    console.log('please try again');
+  } else {
+    console.log(data[0].description);
+  }
+});
+
+// errors and data validation are two different concerns
+
 
