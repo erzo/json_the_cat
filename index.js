46bcc9f779ef996640f78b9665b814d41d0d727b
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-02 23:02:39 +0000

    modified

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 
-// calling the function()
-
 let input = process.argv.slice(2);
 
+// calling the function fetchBreedDescription()
 fetchBreedDescription(input, function(error, description) {
   if (error === null) {
     console.log(description);
